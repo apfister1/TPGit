1b02ee4642f83addbfa26e20e59998fbb42583da
apfister <apfister@btslih.eu>
2021-01-19 14:39:28 +0100

    Ajout d'une ligne permettant d'afficher Bonjour

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 19 janvier 2021, 14:37
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Bonjour" << endl;
+
+    return 0;
+}
+
